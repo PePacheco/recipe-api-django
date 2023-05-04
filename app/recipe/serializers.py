20476d38d7178a7809b4544c1e6739cdb5e3404d
PePacheco <pedro_pacheco98@outlook.com>
2023-05-04 14:19:55 -0300

    Added the possibility to update tags from the recipe endpoint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -5,6 +5,8 @@ from rest_framework import serializers
 
 from core.models import Recipe, Tag
 
+from typing import List, Dict, Any
+
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tags."""
@@ -26,10 +28,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data: dict) -> Recipe:
-        """Create a recipe"""
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags: List[Dict[str, Any]], recipe: Recipe):
+        """Handle getting or creating tags."""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -37,7 +37,26 @@ class RecipeSerializer(serializers.ModelSerializer):
                 **tag,
             )
             recipe.tags.add(tag_obj)
+
+    def create(self, validated_data: dict) -> Recipe:
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
         return recipe
+
+    def update(self, instance: Recipe, validated_data: dict) -> Recipe:
+        """Update a recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
